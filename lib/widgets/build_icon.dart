1f5626b1d98d65f37c6a5fd9c77b35827446d523
zohaibtenX <125541961+zohaibtenX@users.noreply.github.com>
2024-09-19 14:55:54 +0500

    Update build_icon.dart

--- lib/widgets/build_icon.dart
+++ lib/widgets/build_icon.dart
@@ -65,7 +65,7 @@ class SvgViewer extends StatelessWidget {
         this.height,
         this.width,
         this.color,
-        this.fit = BoxFit.contain}) : super(key: key);
+        this.fit = BoxFit.contain});
 
   @override
   Widget build(BuildContext context) {
